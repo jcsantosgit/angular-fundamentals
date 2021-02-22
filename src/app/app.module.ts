c310fe201ec3e437d9cca3797ba918a97eaf590f
Joao Santos <joao@localhost.localdomain>
2021-02-22 00:00:21 -0300

    inclusão de filtro dos cursos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './courses/course-list.components';
+import { CourseListComponent } from './courses/course-list.component';
 import { StarComponent } from './star/app-star.component';
 
 @NgModule({
---
+++ src/app/courses/course-list.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from './course';
+import { CourseService } from './course.service';
+
+@Component(
+    {
+        selector: 'app-course-list',
+        templateUrl: './course-list.component.html'
+    }
+)
+export class CourseListComponent implements OnInit{
+    
+    _courses: Course[] = [];
+    filterCourses: Course[] = [];
+    _filterBy: string = '';
+
+    constructor(private courseService: CourseService){
+
+    }
+
+    ngOnInit(): void {
+        this._courses = this.courseService.retrieveAll();
+        this.filterCourses = this._courses;
+    }
+
+    set filter(value: string){
+        console.log(value);
+        this._filterBy = value;
+        if(value.length > 0)
+            this.filterCourses = this._courses.filter((c: Course) => c.name.toLocaleLowerCase().indexOf(value.toLocaleLowerCase()) > -1)
+        else
+            this.filterCourses = this._courses;
+    }
+
+    get filter(){
+        return this._filterBy;
+    }
+
+}
