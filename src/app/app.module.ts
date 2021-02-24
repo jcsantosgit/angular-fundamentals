ac01e4cc8ce4fb82a8862af68fa9e44dcd19555c
Joao Santos <joao@localhost.localdomain>
2021-02-23 22:28:08 -0300

    inclusão de rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
 import { StarComponent } from './star/app-star.component';
 import { ReplacePipe } from './courses/replace.pipe';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
     StarComponent,
-    ReplacePipe
+    ReplacePipe,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([{
+      path: '',
+      redirectTo: 'courses',
+      pathMatch: 'full'
+    },
+    {
+      path: 'courses',
+      component: CourseListComponent
+    }])
   ],
   providers: [],
   bootstrap: [AppComponent]
